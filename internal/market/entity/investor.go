0c3e91530cc7628398292612bcf4278a4cf62b06
Aquiles <wesley419@gmail.com>
2023-06-19 12:49:38 -0300

    first commit

---
+++ internal/market/entity/investor.go
@@ -0,0 +1,48 @@
+package entity
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) UpdateAssetPosition(assetID string, qtdShares int) {
+	assetPostion := i.GetAssetPosition(assetID)
+	if assetPostion != nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetID, qtdShares))
+	} else {
+		assetPostion.Shares += qtdShares
+	}
+}
+
+func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetId == assetID {
+			return assetPosition
+		}
+	}
+	return nil
+}
+
+type InvestorAssetPosition struct {
+	AssetId string
+	Shares  int
+}
+
+func NewInvestorAssetPosition(assetId string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetId: assetId,
+		Shares:  shares,
+	}
+}
